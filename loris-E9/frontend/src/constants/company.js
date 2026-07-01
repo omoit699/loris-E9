7d808e0df12e561a2b01774eee98cb37bc074853
Omoit699 <lawrenceomoit66@gmail.com>
2026-07-01 16:26:45 +0300

    Refactor company object structure in company.js

--- loris-E9/frontend/src/constants/company.js
+++ loris-E9/frontend/src/constants/company.js
@@ -1,13 +1,48 @@
 const company = {
+
     name: "Loris E9",
-    tagline: "We Value Your Satisfaction",
+
+    slogan: "We Value Your Satisfaction",
+
+    description:
+        "Modern hardware, construction materials, agricultural supplies, plumbing, electrical equipment and industrial products.",
+
     email: "lawrenceomoit66@gmail.com",
-    phone: [
+
+    phones: [
+
         "+256790206354",
+
         "+256787772067"
+
     ],
+
     whatsapp: "+256790206354",
-    logo: "/images/logo.png"
+
+    logo: "/images/logo.png",
+
+    favicon: "/favicon.ico",
+
+    location: "Bukedea, Uganda",
+
+    currency: "UGX",
+
+    supportHours: "Mon - Sat : 8:00 AM - 6:00 PM",
+
+    social: {
+
+        facebook: "",
+
+        instagram: "",
+
+        twitter: "",
+
+        tiktok: "",
+
+        youtube: ""
+
+    }
+
 };
 
 export default company;
